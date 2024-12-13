49e18d3333ea6bdfc3964851eed5b0745486efe8
rawan <rawanhadoud1235@gmail.com>
2024-12-13 22:06:11 +0200

    java

---
+++ java/reports.js
@@ -0,0 +1,44 @@
+document.querySelector(".reph1").style.color = 'black';
+document.querySelector(".reph2").style.color = 'black';
+document.querySelector(".reph3").style.color = 'black';
+document.querySelector(".reph4").style.color = 'black';
+document.querySelector(".reph5").style.color = 'black';
+document.querySelector(".reph6").style.color = 'black';
+document.querySelector(".reph7").style.color = 'black';
+document.querySelector(".reph8").style.color = 'black';
+// ////////////////////////////////////////////////////
+document.querySelector(".repp1").style.color = '#9c9c9c';
+document.querySelector(".repp2").style.color = '#9c9c9c';
+document.querySelector(".repp3").style.color = '#9c9c9c';
+document.querySelector(".repp4").style.color = '#9c9c9c';
+document.querySelector(".repp5").style.color = '#9c9c9c';
+document.querySelector(".repp6").style.color = '#9c9c9c';
+document.querySelector(".repp7").style.color = '#9c9c9c';
+document.querySelector(".repp8").style.color = '#9c9c9c';
+// //////////////////////////////////////////////////////
+document.querySelector(".prij1").style.fontSize = '20px';
+document.querySelector(".prij2").style.fontSize = '20px';
+document.querySelector(".prij3").style.fontSize = '20px';
+document.querySelector(".prij4").style.fontSize = '20px';
+document.querySelector(".prij5").style.fontSize = '20px';
+document.querySelector(".prij6").style.fontSize = '20px';
+document.querySelector(".prij7").style.fontSize = '20px';
+document.querySelector(".prij8").style.fontSize = '20px';
+// //////////////////////////////////////////////////////
+document.querySelector(".repa1").style.color = "rgb(80, 163, 241)";
+document.querySelector(".repa1").style.fontSize = '25px';
+document.querySelector(".repa2").style.color = "rgb(80, 163, 241)";
+document.querySelector(".repa2").style.fontSize = '25px';
+document.querySelector(".repa3").style.color = "rgb(80, 163, 241)";
+document.querySelector(".repa3").style.fontSize = '25px';
+document.querySelector(".repa4").style.color = "rgb(80, 163, 241)";
+document.querySelector(".repa4").style.fontSize = '25px';
+document.querySelector(".repa5").style.color = "rgb(80, 163, 241)";
+document.querySelector(".repa5").style.fontSize = '25px';
+document.querySelector(".repa6").style.color = "rgb(80, 163, 241)";
+document.querySelector(".repa6").style.fontSize = '25px';
+document.querySelector(".repa7").style.color = "rgb(80, 163, 241)";
+document.querySelector(".repa7").style.fontSize = '25px';
+document.querySelector(".repa8").style.color = "rgb(80, 163, 241)";
+document.querySelector(".repa8").style.fontSize = '25px';
+// /////////////////////////////////////////////////////////////////
